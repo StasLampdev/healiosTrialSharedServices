b06d0896a860d112d761827568209e60fafb6ce1
StasLampdev <lampdev.dev7@gmail.com>
2020-12-29 17:34:28 +0200

    Moved services

---
+++ src/GuzzleRequestExceptionTransformer.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace SharedServices;
+
+use GuzzleHttp\Exception\RequestException;
+
+class GuzzleRequestExceptionTransformer
+{
+    /**
+     * @param RequestException $exception
+     * @return string
+     */
+    public static function toString(RequestException $exception): string
+    {
+        $body = (string)$exception->getResponse()->getBody();
+        $body = json_decode($body, true);
+
+        if (is_array($body)) {
+            if (array_key_exists('errors', $body)) {
+                return (string)$body['errors'];
+            }
+
+            if (array_key_exists('message', $body)) {
+                return (string)$body['message'];
+            }
+        }
+
+        return $exception->getMessage();
+    }
+}
